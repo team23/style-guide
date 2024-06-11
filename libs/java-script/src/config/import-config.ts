db786e198dda276ccc03718733464d9078c8c775
Erik Herberg <herberg@team23.de>
2024-06-11 14:51:27 +0200

    fix(java-script): usage of import plugin

--- libs/java-script/src/config/import-config.ts
+++ libs/java-script/src/config/import-config.ts
@@ -1,31 +1,18 @@
 import { Linter } from "eslint";
-// @ts-expect-error no typings
-import importPlugin from "eslint-plugin-import";
+import pluginImport from 'eslint-plugin-import-x';
 import FlatConfig = Linter.FlatConfig;
 
-const importBaseConfig: FlatConfig = {
+const importConfig: FlatConfig = {
+    name: 'team23/java-script/import/recommended',
     plugins: {
-        import: importPlugin,
+        import: pluginImport,
     },
-    // support for non flatConfig plugins: https://github.com/import-js/eslint-plugin-import/issues/2556
-    settings: {
-        "import/parsers": {
-            espree: [".js", ".cjs", ".mjs", ".jsx"],
-        },
-    },
-};
-
-const importModificationConfig: FlatConfig = {
     rules: {
         "import/no-default-export": "error",
         "import/no-deprecated": "error",
-        "import/no-extraneous-dependencies": "off",
-        "import/no-internal-modules": "off",
-        "import/no-unassigned-import": "off",
     },
 };
 
 export default [
-    importBaseConfig,
-    importModificationConfig,
+    importConfig,
 ];
