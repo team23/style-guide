2d3eb9cbb8a4bbf120af45024549656ed99065f3
Arne Brandt <brandt@team23.de>
2024-07-01 17:08:58 +0200

    chore: fix double quotes

--- libs/type-script/src/index.ts
+++ libs/type-script/src/index.ts
@@ -1,5 +1,5 @@
-import tseslint from "typescript-eslint";
-import globals from "globals";
+import tseslint from 'typescript-eslint';
+import globals from 'globals';
 import tseslintConfig from './config/tseslint-config.js';
 import stylisticConfig from './config/stylistic-config.js';
 import tseslintStylisticConfig from './config/tseslint-stylistic-config.js';
@@ -9,10 +9,10 @@ import FlatConfig = Linter.FlatConfig;
 const baseConfig = tseslint.config({
     name: 'team23/type-script/core/base',
     ignores: [
-        "polyfills.ts",
-        "jest.config.ts",
-        "dist/**",
-        "node_modules/**",
+        'polyfills.ts',
+        'jest.config.ts',
+        'dist/**',
+        'node_modules/**',
     ],
     languageOptions: {
         globals: {
@@ -20,9 +20,9 @@ const baseConfig = tseslint.config({
             ...globals.es2021,
         },
         parserOptions: {
-            ecmaVersion: "latest",
-            sourceType: "module",
-            project: ["tsconfig.?*.json"],
+            ecmaVersion: 'latest',
+            sourceType: 'module',
+            project: ['tsconfig.?*.json'],
         },
     },
 });
@@ -30,25 +30,25 @@ const baseConfig = tseslint.config({
 const fileBasedModificationConfig = tseslint.config(
     {
         name: 'team23/type-script/core/file-based/cypress',
-        files: ["cypress.config.ts", "cypress.**.config.ts"],
+        files: ['cypress.config.ts', 'cypress.**.config.ts'],
         rules: {
-            "no-console": "off",
+            'no-console': 'off',
         },
     },
     {
         name: 'team23/type-script/core/file-based/page-object',
-        files: ["*.po.ts"],
+        files: ['*.po.ts'],
         rules: {
-            "@typescript-eslint/explicit-function-return-type": "off",
+            '@typescript-eslint/explicit-function-return-type': 'off',
         },
     },
     {
         name: 'team23/type-script/core/file-based/mocks',
-        files: ["*.spec.ts", "*.spec.tsx", "**/__mocks__/**/*.ts", "**/__mocks__/**/*.tsx"],
+        files: ['*.spec.ts', '*.spec.tsx', '**/__mocks__/**/*.ts', '**/__mocks__/**/*.tsx'],
         rules: {
-            "@typescript-eslint/no-magic-numbers": "off",
-            "@typescript-eslint/no-empty-function": "off",
-            "max-lines": "off",
+            '@typescript-eslint/no-magic-numbers': 'off',
+            '@typescript-eslint/no-empty-function': 'off',
+            'max-lines': 'off',
         },
     },
 );
